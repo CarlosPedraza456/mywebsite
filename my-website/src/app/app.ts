b02ff3e7937beaf18e5a262b6ce4660300bb4370
Carlos Pedraza Romero <carlosdavid.cd23@gmail.com>
2025-11-04 17:11:35 -0400

    about-me-feature: adding all skills and resume of me

--- my-website/src/app/app.ts
+++ my-website/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Hero } from "./components/hero/hero";
 import { FlareShadow } from './components/flare-shadow/flare-shadow';
+import { AboutMe } from './components/about-me/about-me';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero, FlareShadow],
+  imports: [RouterOutlet, Header, Hero, FlareShadow, AboutMe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
