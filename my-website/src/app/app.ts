dbaba4959857d2ed8b0ef31257fe1506b6e42864
Carlos Pedraza Romero <carlosdavid.cd23@gmail.com>
2025-11-04 12:55:50 -0400

    flares-feature: added a grayscale shadow to the background

--- my-website/src/app/app.ts
+++ my-website/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { Hero } from "./components/hero/hero";
+import { FlareShadow } from './components/flare-shadow/flare-shadow';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, FlareShadow],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
